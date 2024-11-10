f4ee1f8878d225a79782cc13a769f66e323599e3
kite_U <109857975+youyeon11@users.noreply.github.com>
2024-11-11 02:08:51 +0900

    [base] api 모듈 생성

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
@@ -5,17 +7,37 @@ plugins {
     id("io.spring.dependency-management") version "1.1.6"
 }
 
-group = "com.zerobase"
-version = "0.0.1-SNAPSHOT"
+repositories {
+    mavenCentral()
+}
 
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
+allprojects {
+    group = "com.zerobase"
+
+    repositories {
+        mavenCentral()
     }
 }
 
-repositories {
-    mavenCentral()
+subprojects {
+    apply {
+        plugin("org.jetbrains.kotlin.jvm")
+        plugin("org.jetbrains.kotlin.plugin.spring")
+
+        plugin("org.springframework.boot")
+        plugin("io.spring.dependency-management")
+    }
+
+    tasks.withType<KotlinCompile>().configureEach {
+        kotlinOptions {
+            freeCompilerArgs += "-Xjsr305=strict"
+            jvmTarget = "17"
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
 }
 
 dependencies {
@@ -24,14 +46,4 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-kotlin {
-    compilerOptions {
-        freeCompilerArgs.addAll("-Xjsr305=strict")
-    }
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,2 @@
 rootProject.name = "fintech"
+include("api")
