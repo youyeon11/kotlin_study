679ffb16eb17429558a3b19449da7c0cf559bbda
kite_U <yurry45@naver.com>
2025-02-13 13:49:43 +0900

    init: 초기 세팅

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -8,7 +8,7 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.7.6")
 
-    implementation("io.springfox:springfox-boot-starter:3.0.0")
-
     implementation(project(":domain"))
+
+    implementation("io.springfox:springfox-boot-starter:3.0.0")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.6.0"
     kotlin("plugin.spring") version "1.6.10" apply false
-    kotlin("plugin.jpa") version "1.6.0" apply false
+    kotlin("plugin.jpa") version "1.6.10" apply false
 
     id("org.springframework.boot") version "2.6.3" apply false
     id("io.spring.dependency-management") version "1.0.11.RELEASE" apply false
