b4c63ae921803499221b0ca508b381f172782716
kite_U <yurry45@naver.com>
2025-02-19 16:33:19 +0900

    base: test framework dependency 추가

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -11,4 +11,9 @@ dependencies {
     implementation(project(":domain"))
 
     implementation("io.springfox:springfox-boot-starter:3.0.0")
+
+    // Mock처리를 하는 의존성(kotlin의 test framework)
+    implementation("io.mockk:mockk:1.12.0")
+    // test code에서의 DB connection(외부 말고 인메모리 DB 테스트)
+    runtimeOnly("com.h2database:h2")
 }
